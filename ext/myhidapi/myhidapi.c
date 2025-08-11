0d7e3e76c93b79cfb523d72bf12eb77c7a1f83fb
Repy <gamecube02+git@gmail.com>
2025-08-11 09:49:12 +0900

    clang-format

--- ext/myhidapi/myhidapi.c
+++ ext/myhidapi/myhidapi.c
@@ -3,16 +3,20 @@
 VALUE mMyHIDAPI;
 VALUE cMyHIDAPIHandle;
 
-static void dealloc(void * ptr)
-{
+static void dealloc(void *ptr) {
     hid_device *handle = (hid_device *)ptr;
     hid_close(handle);
 }
 
 static const rb_data_type_t myhidapi_handle_type = {
     "MyHIDAPI/Handle",
-    {0, dealloc, 0,},
-    0, 0,
+    {
+        0,
+        dealloc,
+        0,
+    },
+    0,
+    0,
 #ifdef RUBY_TYPED_FREE_IMMEDIATELY
     RUBY_TYPED_FREE_IMMEDIATELY,
 #endif
@@ -20,22 +24,18 @@ static const rb_data_type_t myhidapi_handle_type = {
 
 #define BUF_SIZE 4096
 
-static VALUE
-rb_wcstombs(wchar_t *str)
-{
+static VALUE rb_wcstombs(wchar_t *str) {
     char buf[BUF_SIZE];
     int len;
     len = wcstombs(buf, str, BUF_SIZE);
     if (len > 0) {
-	return rb_str_new(buf, len);
+        return rb_str_new(buf, len);
     } else {
-	return Qnil;
+        return Qnil;
     }
 }
 
-static VALUE
-enumerate(VALUE mod, VALUE vendor_id, VALUE product_id)
-{
+static VALUE enumerate(VALUE mod, VALUE vendor_id, VALUE product_id) {
     VALUE devices;
     struct hid_device_info *devs, *cur_dev;
 
@@ -43,54 +43,45 @@ enumerate(VALUE mod, VALUE vendor_id, VALUE product_id)
     devs = hid_enumerate(NUM2USHORT(vendor_id), NUM2USHORT(product_id));
     cur_dev = devs;
     while (cur_dev) {
-	rb_ary_push(devices, rb_funcall(mMyHIDAPI, rb_intern("build_device_info"), 8,
-		INT2NUM(cur_dev->vendor_id),
-		INT2NUM(cur_dev->product_id),
-		rb_str_new2(cur_dev->path),
-		rb_wcstombs(cur_dev->serial_number),
-		rb_wcstombs(cur_dev->manufacturer_string),
-		rb_wcstombs(cur_dev->product_string),
-		INT2NUM(cur_dev->usage),
-		INT2NUM(cur_dev->interface_number)
-		));
-	cur_dev = cur_dev->next;
+        rb_ary_push(devices,
+                    rb_funcall(mMyHIDAPI, rb_intern("build_device_info"), 8,
+                               INT2NUM(cur_dev->vendor_id), INT2NUM(cur_dev->product_id),
+                               rb_str_new2(cur_dev->path), rb_wcstombs(cur_dev->serial_number),
+                               rb_wcstombs(cur_dev->manufacturer_string),
+                               rb_wcstombs(cur_dev->product_string), INT2NUM(cur_dev->usage),
+                               INT2NUM(cur_dev->interface_number)));
+        cur_dev = cur_dev->next;
     }
     hid_free_enumeration(devs);
 
     return devices;
 }
 
-static VALUE
-rb_hid_open(VALUE mod, VALUE vid, VALUE pid)
-{
+static VALUE rb_hid_open(VALUE mod, VALUE vid, VALUE pid) {
     hid_device *handle;
 
     handle = hid_open(NUM2USHORT(vid), NUM2USHORT(pid), NULL);
 
     if (handle) {
-	return TypedData_Wrap_Struct(cMyHIDAPIHandle, &myhidapi_handle_type, handle);
+        return TypedData_Wrap_Struct(cMyHIDAPIHandle, &myhidapi_handle_type, handle);
     } else {
-	return Qfalse;
+        return Qfalse;
     }
 }
 
-static VALUE
-rb_hid_open_path(VALUE mod, VALUE path)
-{
+static VALUE rb_hid_open_path(VALUE mod, VALUE path) {
     hid_device *handle;
 
     handle = hid_open_path(StringValueCStr(path));
 
     if (handle) {
-	return TypedData_Wrap_Struct(cMyHIDAPIHandle, &myhidapi_handle_type, handle);
+        return TypedData_Wrap_Struct(cMyHIDAPIHandle, &myhidapi_handle_type, handle);
     } else {
-	return Qfalse;
+        return Qfalse;
     }
 }
 
-static VALUE
-rb_hid_write(VALUE self, VALUE str)
-{
+static VALUE rb_hid_write(VALUE self, VALUE str) {
     hid_device *handle;
     int written;
 
@@ -99,29 +90,25 @@ rb_hid_write(VALUE self, VALUE str)
     written = hid_write(handle, (unsigned char *)StringValuePtr(str), RSTRING_LEN(str));
 
     if (written >= 0) {
-	return INT2NUM(written);
+        return INT2NUM(written);
     } else {
-	return Qfalse;
+        return Qfalse;
     }
 }
 
-static VALUE
-rb_hid_set_nonblocking(VALUE self, VALUE val)
-{
+static VALUE rb_hid_set_nonblocking(VALUE self, VALUE val) {
     hid_device *handle;
     TypedData_Get_Struct(self, hid_device, &myhidapi_handle_type, handle);
     if (!hid_set_nonblocking(handle, NUM2INT(val))) {
-	return Qtrue;
+        return Qtrue;
     } else {
-	return Qnil;
+        return Qnil;
     }
 }
 
-static VALUE
-rb_hid_read(VALUE self, VALUE size)
-{
+static VALUE rb_hid_read(VALUE self, VALUE size) {
     hid_device *handle;
-    unsigned char * buf;
+    unsigned char *buf;
     int read;
     VALUE ret;
 
@@ -132,19 +119,17 @@ rb_hid_read(VALUE self, VALUE size)
     read = hid_read(handle, buf, NUM2SIZET(size));
 
     if (read > 0) {
-	ret = rb_str_new((char *)buf, read);
+        ret = rb_str_new((char *)buf, read);
     } else {
-	ret = Qnil;
+        ret = Qnil;
     }
     xfree(buf);
     return ret;
 }
 
-static VALUE
-rb_hid_read_timeout(VALUE self, VALUE size, VALUE timeout_ms)
-{
+static VALUE rb_hid_read_timeout(VALUE self, VALUE size, VALUE timeout_ms) {
     hid_device *handle;
-    unsigned char * buf;
+    unsigned char *buf;
     int read;
     VALUE ret;
 
@@ -155,17 +140,15 @@ rb_hid_read_timeout(VALUE self, VALUE size, VALUE timeout_ms)
     read = hid_read_timeout(handle, buf, NUM2SIZET(size), NUM2INT(timeout_ms));
 
     if (read > 0) {
-	ret = rb_str_new((char *)buf, read);
+        ret = rb_str_new((char *)buf, read);
     } else {
-	ret = Qnil;
+        ret = Qnil;
     }
     xfree(buf);
     return ret;
 }
 
-static VALUE
-rb_manufacturer(VALUE self)
-{
+static VALUE rb_manufacturer(VALUE self) {
     hid_device *handle;
     TypedData_Get_Struct(self, hid_device, &myhidapi_handle_type, handle);
 
@@ -175,9 +158,7 @@ rb_manufacturer(VALUE self)
     return rb_wcstombs(buffer);
 }
 
-static VALUE
-rb_product(VALUE self)
-{
+static VALUE rb_product(VALUE self) {
     hid_device *handle;
     TypedData_Get_Struct(self, hid_device, &myhidapi_handle_type, handle);
 
